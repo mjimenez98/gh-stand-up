d11b7f3307dbd17f36ec76ac89f9b9b32a15ed16
Miguel Jimenez <mjimenez98@github.com>
2025-04-17 20:36:51 +0000

    add tests for generating today's report

--- internal/report/report_test.go
+++ internal/report/report_test.go
@@ -12,7 +12,7 @@ func TestGenerateYesterdayReport(t *testing.T) {
 	t.Run("When no activity", func(t *testing.T) {
 		generatedReport := GenerateYesterdayReport([]github.Issue{}, []github.PullRequest{}, []github.PullRequest{})
 
-		assert.Equal(t, "No activity detected yesterday.\n", generatedReport, "Generated report does not match expected output when no activity")
+		assert.Equal(t, "No activity detected yesterday.\n", generatedReport)
 	})
 
 	t.Run("When issues were opened", func(t *testing.T) {
@@ -23,9 +23,9 @@ func TestGenerateYesterdayReport(t *testing.T) {
 
 		generatedReport := GenerateYesterdayReport(openedIssues, []github.PullRequest{}, []github.PullRequest{})
 
-		assert.Contains(t, generatedReport, "This is what you did yesterday:\n", "Generated report does not contain the expected header")
-		assert.Contains(t, generatedReport, "📋 Opened Open issue - https://api.github.com/example/repo/issues/1", "Generated report does not contain the expected opened issue")
-		assert.Contains(t, generatedReport, "📋 Opened Another issue - https://api.github.com/example/repo/issues/2", "Generated report does not contain the expected opened issue")
+		assert.Contains(t, generatedReport, "This is what you did yesterday:\n")
+		assert.Contains(t, generatedReport, "📋 Opened Open issue - https://api.github.com/example/repo/issues/1")
+		assert.Contains(t, generatedReport, "📋 Opened Another issue - https://api.github.com/example/repo/issues/2")
 	})
 
 	t.Run("When pull requests were opened", func(t *testing.T) {
@@ -36,9 +36,9 @@ func TestGenerateYesterdayReport(t *testing.T) {
 
 		generatedReport := GenerateYesterdayReport([]github.Issue{}, openedPullRequests, []github.PullRequest{})
 
-		assert.Contains(t, generatedReport, "This is what you did yesterday:\n", "Generated report does not contain the expected header")
-		assert.Contains(t, generatedReport, "🔀 Opened Open PR - https://api.github.com/example/repo/pull/1", "Generated report does not contain the expected opened pull request")
-		assert.Contains(t, generatedReport, "🔀 Opened Another PR - https://api.github.com/example/repo/pull/2", "Generated report does not contain the expected opened pull request")
+		assert.Contains(t, generatedReport, "This is what you did yesterday:\n")
+		assert.Contains(t, generatedReport, "🔀 Opened Open PR - https://api.github.com/example/repo/pull/1")
+		assert.Contains(t, generatedReport, "🔀 Opened Another PR - https://api.github.com/example/repo/pull/2")
 	})
 
 	t.Run("When pull requests were reviewed", func(t *testing.T) {
@@ -48,7 +48,28 @@ func TestGenerateYesterdayReport(t *testing.T) {
 
 		generatedReport := GenerateYesterdayReport([]github.Issue{}, []github.PullRequest{}, pullRequestsReviewed)
 
-		assert.Contains(t, generatedReport, "This is what you did yesterday:\n", "Generated report does not contain the expected header")
-		assert.Contains(t, generatedReport, "🔍 Reviewed Reviewed PR - https://api.github.com/example/repo/pull/3", "Generated report does not contain the expected reviewed pull request")
+		assert.Contains(t, generatedReport, "This is what you did yesterday:\n")
+		assert.Contains(t, generatedReport, "🔍 Reviewed Reviewed PR - https://api.github.com/example/repo/pull/3")
+	})
+}
+
+func TestGenerateTodayReport(t *testing.T) {
+	t.Run("When no activity", func(t *testing.T) {
+		generatedReport := GenerateTodayReport([]github.PullRequest{})
+
+		assert.Equal(t, "No open pull requests to work on.\n", generatedReport)
+	})
+
+	t.Run("When pull requests are open", func(t *testing.T) {
+		openPullRequests := []github.PullRequest{
+			{Title: "Open PR", URL: "https://api.github.com/example/repo/pull/1"},
+			{Title: "Another PR", URL: "https://api.github.com/example/repo/pull/2"},
+		}
+
+		generatedReport := GenerateTodayReport(openPullRequests)
+
+		assert.Contains(t, generatedReport, "This is work you have in progress or have yet to address:\n")
+		assert.Contains(t, generatedReport, "🔨 Work on Open PR - https://api.github.com/example/repo/pull/1")
+		assert.Contains(t, generatedReport, "🔨 Work on Another PR - https://api.github.com/example/repo/pull/2")
 	})
 }
